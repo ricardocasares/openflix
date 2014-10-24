f9c256343db2e2d161eb641d1694fde4b5ac475e
Ricardo Casares <rcasares@bhp-global.com>
2014-10-24 17:25:50 -0300

    lib path

--- server.js
+++ server.js
@@ -2,15 +2,15 @@
 var express = require('express');
 var log = require('debug')('app:server');
 // relative dependencies
-var errors = require('lib/errors');
-var routes = require('lib/routes');
+var errors = require('./lib/errors');
+var routes = require('./lib/routes');
 // app instance
 var app = express();
 // app settings
 app.set('port', process.env.PORT || 3000);
-app.set('views', 'lib/views');
+app.set('views', './lib/views');
 app.set('view engine', 'jade');
-app.use(express.static(__dirname + '/public'));
+app.use(express.static(__dirname + './public'));
 // app modules
 app.use('/views', routes.views);
 app.use('*', routes.home);
